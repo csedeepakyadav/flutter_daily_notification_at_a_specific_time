744042d46e157c96f1b93345052126a2d15dfb3a
Deepak Yadav <deepak.yadav@teamcoda.com>
2021-12-02 23:21:02 +0530

    binding fix, sound added

--- lib/local_notification_repo.dart
+++ lib/local_notification_repo.dart
@@ -5,10 +5,26 @@ import 'package:timezone/data/latest_all.dart' as tz;
 import 'package:timezone/timezone.dart' as tz;
 
 class LocalNotificationRepo {
-  FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+  FlutterLocalNotificationsPlugin? flutterLocalNotificationsPlugin =
       FlutterLocalNotificationsPlugin();
 
   initNoti() async {
+// initialise the plugin. app_icon needs to be a added as a drawable resource to the Android head project
+    // const AndroidInitializationSettings initializationSettingsAndroid =
+    //     AndroidInitializationSettings('@mipmap/ic_launcher');
+    // final IOSInitializationSettings initializationSettingsIOS =
+    //     IOSInitializationSettings(
+    //         onDidReceiveLocalNotification: onDidReceiveLocalNotification);
+    // final MacOSInitializationSettings initializationSettingsMacOS =
+    //     MacOSInitializationSettings();
+    // final InitializationSettings initializationSettings =
+    //     InitializationSettings(
+    //         android: initializationSettingsAndroid,
+    //         iOS: initializationSettingsIOS,
+    //         macOS: initializationSettingsMacOS);
+    // await flutterLocalNotificationsPlugin!.initialize(initializationSettings,
+    //     onSelectNotification: selectNotification);
+
 // initialise the plugin. app_icon needs to be a added as a drawable resource to the Android head project
     const AndroidInitializationSettings initializationSettingsAndroid =
         AndroidInitializationSettings('@mipmap/ic_launcher');
@@ -22,26 +38,33 @@ class LocalNotificationRepo {
             android: initializationSettingsAndroid,
             iOS: initializationSettingsIOS,
             macOS: initializationSettingsMacOS);
-    await flutterLocalNotificationsPlugin.initialize(initializationSettings,
+    await flutterLocalNotificationsPlugin!.initialize(initializationSettings,
         onSelectNotification: selectNotification);
   }
 
   Future<void> configureLocalTimeZone() async {
-    tz.initializeTimeZones();
-    final String? timeZoneName = await FlutterNativeTimezone.getLocalTimezone();
-    tz.setLocalLocation(tz.getLocation(timeZoneName!));
+    try {
+      tz.initializeTimeZones();
+      final String? timeZoneName =
+          await FlutterNativeTimezone.getLocalTimezone();
+      tz.setLocalLocation(tz.getLocation(timeZoneName!));
+    } catch (e) {
+      print("error: $e");
+    }
   }
 
   showLocalNoti({@required String? message}) async {
     const AndroidNotificationDetails androidPlatformChannelSpecifics =
         AndroidNotificationDetails('your channel id', 'your channel name',
             channelDescription: 'your channel description',
+            sound: RawResourceAndroidNotificationSound('slow_spring_board'),
+            playSound: true,
             importance: Importance.max,
             priority: Priority.high,
             ticker: 'ticker');
     const NotificationDetails platformChannelSpecifics =
         NotificationDetails(android: androidPlatformChannelSpecifics);
-    await flutterLocalNotificationsPlugin.show(
+    await flutterLocalNotificationsPlugin!.show(
         0, 'test App', message, platformChannelSpecifics,
         payload: 'item x');
   }
@@ -59,15 +82,16 @@ class LocalNotificationRepo {
 
 // daily notification at 10 AM
   Future<void> scheduleDailyTenAMNotification() async {
-    await flutterLocalNotificationsPlugin.zonedSchedule(
+    await flutterLocalNotificationsPlugin!.zonedSchedule(
         0,
-        'daily scheduled notification title',
-        'daily scheduled notification body',
+        '10 AM ',
+        'fired at 10 PM',
         _nextInstanceOfTenAM(),
         const NotificationDetails(
-          android: AndroidNotificationDetails('daily notification channel id',
-              'daily notification channel name',
-              channelDescription: 'daily notificationdescription'),
+          android: AndroidNotificationDetails('daily 111', 'daily 11',
+              sound: RawResourceAndroidNotificationSound('slow_spring_board'),
+              playSound: true,
+              channelDescription: 'daily  1'),
         ),
         androidAllowWhileIdle: true,
         uiLocalNotificationDateInterpretation:
@@ -90,15 +114,16 @@ class LocalNotificationRepo {
 
   // daily notification at 10 AM
   Future<void> scheduleDailyT1PMNotification() async {
-    await flutterLocalNotificationsPlugin.zonedSchedule(
+    await flutterLocalNotificationsPlugin!.zonedSchedule(
         0,
-        'daily scheduled notification title',
-        'daily scheduled notification body',
+        '1 PM ',
+        'fired at 1 pm',
         _nextInstanceOf1Pm(),
         const NotificationDetails(
-          android: AndroidNotificationDetails('daily notification channel id',
-              'daily notification channel name',
-              channelDescription: 'daily notificationdescription'),
+          android: AndroidNotificationDetails('daily 444', 'daily 44',
+              sound: RawResourceAndroidNotificationSound('slow_spring_board'),
+              playSound: true,
+              channelDescription: 'daily  4'),
         ),
         androidAllowWhileIdle: true,
         uiLocalNotificationDateInterpretation:
@@ -121,15 +146,16 @@ class LocalNotificationRepo {
 
   // daily notification at 10 AM
   Future<void> scheduleDailyT3PMNotification() async {
-    await flutterLocalNotificationsPlugin.zonedSchedule(
+    await flutterLocalNotificationsPlugin!.zonedSchedule(
         0,
-        'daily scheduled notification title',
-        'daily scheduled notification body',
+        '3 PM ',
+        'fired at 3 pm',
         _nextInstanceOf3Pm(),
         const NotificationDetails(
-          android: AndroidNotificationDetails('daily notification channel id',
-              'daily notification channel name',
-              channelDescription: 'daily notificationdescription'),
+          android: AndroidNotificationDetails('daily 222', 'daily 22',
+              sound: RawResourceAndroidNotificationSound('slow_spring_board'),
+              playSound: true,
+              channelDescription: 'daily  2'),
         ),
         androidAllowWhileIdle: true,
         uiLocalNotificationDateInterpretation:
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:periodicnotificationdaily/local_notification_repo.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  LocalNotificationRepo notificationRepo = LocalNotificationRepo();
+
+  await notificationRepo.configureLocalTimeZone();
+  notificationRepo.initNoti();
+  notificationRepo.showLocalNoti(message: "app just opened");
+  notificationRepo.scheduleDailyTenAMNotification();
+  notificationRepo.scheduleDailyT1PMNotification();
+  notificationRepo.scheduleDailyT3PMNotification();
+
   runApp(const MyApp());
 }
 
@@ -35,19 +45,19 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   void initState() {
     super.initState();
-    notificationRepo.configureLocalTimeZone().then((value) {
-      notificationRepo.initNoti();
-      notificationRepo.scheduleDailyTenAMNotification();
-      notificationRepo.scheduleDailyT1PMNotification();
-      notificationRepo.scheduleDailyT3PMNotification();
-    });
   }
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("Daily periodic notification at 10:00 AM"),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Notifications"),
+      ),
+      body: const Center(
+        child: Text(
+          "Daily periodic notification at multiple time",
+          textAlign: TextAlign.center,
+        ),
       ),
     );
   }
